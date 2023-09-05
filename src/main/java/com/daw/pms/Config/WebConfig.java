848c05e632039ff9f19dab4d7b01f8d362e03bd0
Daw Loph <loph3xertoi@gmail.com>
2023-09-05 14:57:04 +0800

    Fix bug: Enable access of static resources.
    Signed-off-by: Daw Loph <loph3xertoi@gmail.com>

--- src/main/java/com/daw/pms/Config/WebConfig.java
+++ src/main/java/com/daw/pms/Config/WebConfig.java
@@ -1,15 +1,18 @@
 package com.daw.pms.Config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
-@EnableWebMvc
+// @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
   }
+
+//  @Override
+//  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//    registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+//  }
 }
